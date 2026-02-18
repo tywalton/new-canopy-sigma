c358bf2fbbf148e6a3b7f774c6622a6db7352c8d
tywalton <tyreewalton2027@u.northwestern.edu>
2026-02-18 14:06:06 -0600

    Update SigmaExample.Client.tsx

--- app/components/SigmaExample.Client.tsx
+++ app/components/SigmaExample.Client.tsx
@@ -22,10 +22,10 @@ export default function SigmaExample({...rest}) {
 
       const themes = [
         {id: "theme1", label: "Water Systems", count: 10},
-        {id: "theme2", label: "Environmental Impacts on Wildlife and Natural Areas", count: 30},
-        {id: "theme3", label: "Energy Systems and Power Infrastructure", count: 20},
+        {id: "theme2", label: "Environmental Impacts on Wildlife and Natural Areas", count: 20},
+        {id: "theme3", label: "Energy Systems and Power Infrastructure", count: 30},
         {id: "theme4", label: "Transportation Infrastructure and Mobility", count: 40},
-        {id: "theme5", label: "Urban Development, Renewal, and Demolition", count: 40},
+        {id: "theme5", label: "Urban Development, Renewal, and Demolition", count: 50},
       ];
 
       const graph = new Graph();
