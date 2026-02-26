347092f2a497facc408cca0fe2df2a9dfcfc1992
tywalton <tyreewalton2027@u.northwestern.edu>
2026-02-26 13:14:35 -0600

    Update SigmaExample.Client.tsx

--- app/components/SigmaExample.Client.tsx
+++ app/components/SigmaExample.Client.tsx
@@ -63,10 +63,7 @@ const fixedPositions = {
 };
 
 // add nodes  
-function seededRandom(seed) {
-  let x = Math.sin(seed) * 10000;
-  return x - Math.floor(x);
-}
+
       
 themes.forEach((theme) => {
   if (!graph.hasNode(theme.id)) {
